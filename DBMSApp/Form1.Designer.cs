7d61def68da5008dba096f8774c0d14f50e45093
Boldaș Daniel-Alexandru <alexboldas@gmail.com>
2024-05-17 16:09:33 +0300

    Initial commit

---
+++ DBMSApp/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DBMSApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Manufacturers = new System.Windows.Forms.DataGridView();
+            this.Laptops = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Manufacturers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Laptops)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Manufacturers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(694, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Laptops";
+            // 
+            // Manufacturers
+            // 
+            this.Manufacturers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Manufacturers.Location = new System.Drawing.Point(12, 115);
+            this.Manufacturers.Name = "Manufacturers";
+            this.Manufacturers.RowHeadersWidth = 51;
+            this.Manufacturers.RowTemplate.Height = 24;
+            this.Manufacturers.Size = new System.Drawing.Size(422, 276);
+            this.Manufacturers.TabIndex = 2;
+            // 
+            // Laptops
+            // 
+            this.Laptops.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Laptops.Location = new System.Drawing.Point(440, 115);
+            this.Laptops.Name = "Laptops";
+            this.Laptops.RowHeadersWidth = 51;
+            this.Laptops.RowTemplate.Height = 24;
+            this.Laptops.Size = new System.Drawing.Size(543, 276);
+            this.Laptops.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(989, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 276);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1233, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Laptops";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1549, 528);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Laptops);
+            this.Controls.Add(this.Manufacturers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Manufacturers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Laptops)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView Manufacturers;
+        private System.Windows.Forms.DataGridView Laptops;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DBMSApp/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace DBMSApp
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = ConfigurationManager.AppSettings["connectionString"];
+        SqlConnection connection;
+        SqlDataAdapter adapterParent, adapterChild, adapterSecondParent;
+        DataSet dataSet;
+        BindingSource bindingSourceParent, bindingSourceChild, bindingSourceSecondParent;
+        string parentTable = ConfigurationManager.AppSettings["parent"];
+        string childTable = ConfigurationManager.AppSettings["child"];
+        string secondParentTable = ConfigurationManager.AppSettings["secondParent"];
+
+        SqlCommandBuilder cmdBuilder;
+
+        string queryParent = "SELECT * FROM " + ConfigurationManager.AppSettings["parent"];
+        string queryChild = "SELECT * FROM " + ConfigurationManager.AppSettings["child"];
+        string querySecondParent = "SELECT * FROM " + ConfigurationManager.AppSettings["secondParent"];
+        
+        public Form1()
+        {
+            InitializeComponent();
+            FillData();
+        }
+
+        string GetPrimaryKey(string tableName)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT  COL_NAME(ic.OBJECT_ID,ic.column_id) AS ColumnName FROM    sys.indexes AS i INNER JOIN sys.index_columns AS ic ON  i.OBJECT_ID = ic.OBJECT_ID AND i.index_id = ic.index_id WHERE   i.is_primary_key = 1 AND OBJECT_NAME(ic.OBJECT_ID) = '" + tableName + "'", connection);
+            return cmd.ExecuteScalar().ToString();
+        }
+
+        void FillData(){
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+
+            label1.Text = parentTable;
+
+            label2.Text = childTable;
+
+            label3.Text = secondParentTable;
+
+            adapterParent = new SqlDataAdapter(queryParent, connection);
+            adapterChild = new SqlDataAdapter(queryChild, connection);
+            
+            dataSet = new DataSet();
+            if (secondParentTable == "")
+            {
+                adapterParent.Fill(dataSet, parentTable);
+                adapterChild.Fill(dataSet, childTable);
+
+                cmdBuilder = new SqlCommandBuilder(adapterChild);
+
+                //get the primary key of the parent table from information schema
+                string parentPK = GetPrimaryKey(parentTable);
+
+                dataSet.Relations.Add("Parent_Child",
+                    dataSet.Tables[parentTable].Columns[parentPK],
+                    dataSet.Tables[childTable].Columns[parentPK]
+                );
+
+                bindingSourceParent = new BindingSource();
+                bindingSourceParent.DataSource = dataSet.Tables[parentTable];
+
+                bindingSourceChild = new BindingSource();
+                bindingSourceChild.DataSource = bindingSourceParent;
+                bindingSourceChild.DataMember = "Parent_Child";
+
+                this.Manufacturers.DataSource = bindingSourceParent;
+                this.Laptops.DataSource = bindingSourceChild;
+
+                cmdBuilder.GetUpdateCommand();
+                button1.Click += button1_Click;
+            }else{
+                adapterSecondParent = new SqlDataAdapter(querySecondParent, connection);
+                adapterParent.Fill(dataSet, parentTable);
+                adapterChild.Fill(dataSet, childTable);
+                adapterSecondParent.Fill(dataSet, secondParentTable);
+
+                cmdBuilder = new SqlCommandBuilder(adapterChild);
+
+                //get the primary key of the parent table from information schema
+                string parentPK = GetPrimaryKey(parentTable);
+                string secondParentPK = GetPrimaryKey(secondParentTable);
+
+                Manufacturers.DataSource = dataSet.Tables[parentTable];
+                dataGridView1.DataSource = dataSet.Tables[secondParentTable];
+                Laptops.DataSource = dataSet.Tables[childTable];
+
+                Manufacturers.SelectionChanged += (s, e) =>
+                {
+                    if (Manufacturers.SelectedRows.Count > 0)
+                    {
+                        string selectedValue = Manufacturers.SelectedRows[0].Cells[parentPK].Value.ToString();
+                        bindingSourceChild.Filter = "'"+parentPK + " = " + selectedValue+"'";
+                    }
+                };
+                dataGridView1.SelectionChanged += (s, e) =>
+                {
+                    if (dataGridView1.SelectedRows.Count > 0)
+                    {
+                        string selectedValue = dataGridView1.SelectedRows[0].Cells[secondParentPK].Value.ToString();
+                        bindingSourceChild.Filter = "'" + secondParentPK + " = " + selectedValue + "'";
+                    }
+                };
+
+                cmdBuilder.GetUpdateCommand();
+                button1.Click += button1_Click;
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try { 
+                adapterChild.Update(dataSet, childTable);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
